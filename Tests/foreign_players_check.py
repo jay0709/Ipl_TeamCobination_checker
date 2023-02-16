ee936ef71d09396306f6648ae87851b48c27c5dc
Jayabrata <jayabrata.btech0711@gmail.com>
2023-02-16 11:40:16 +0530

    3rd Commit from remote

---
+++ Tests/foreign_players_check.py
@@ -0,0 +1,19 @@
+import json
+import sys
+
+from InputData.parse_team_json import check_max_four_foreign_player
+
+file_path = sys.argv[1]  # gets the filepath from console
+file = open(file_path)
+data = json.load(file)
+
+
+def test_max_foreign_players():
+    check_foreign_players_count = check_max_four_foreign_player(data)
+    assert check_foreign_players_count <= 4, 'You cannnot have more than 4 foreign player in the team..!'
+
+
+file.close()
+
+if __name__ == '__main__':
+    test_max_foreign_players()
--- Tests/wicket_keeper_check.py
+++ Tests/wicket_keeper_check.py
@@ -1,14 +1,14 @@
 import json
 import sys
 
-from parse_team_json import check_min_one_wicket_keeper
+from InputData.parse_team_json import check_min_one_wicket_keeper
 
 file_path = sys.argv[1]  # gets the filepath from console
 file = open(file_path)
 data = json.load(file)
 
 
-def test_wicket_keeper():  # checks for wicket keeper logic
+def test_min_wicket_keeper():  # checks for wicket keeper logic
     check_wk_count = check_min_one_wicket_keeper(data)
     assert check_wk_count > 0, "Need At leas one wicket keeper in the team..!"
 
@@ -16,4 +16,4 @@ def test_wicket_keeper():  # checks for wicket keeper logic
 file.close()
 
 if __name__ == '__main__':
-    test_wicket_keeper()
+    test_min_wicket_keeper()
